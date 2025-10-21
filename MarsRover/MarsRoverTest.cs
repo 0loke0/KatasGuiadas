962e276fc6f25fbc97c82a95e678afdcfe3ee297
ronal.castano <ronal.castano@sinco.com.co>
2025-10-21 11:19:16 -0500

    Se prueba que la meseta tenga las dimensiones especificadas

---
+++ MarsRover/MarsRoverTest.cs
@@ -0,0 +1,35 @@
+﻿using AwesomeAssertions;
+
+namespace MarsRover;
+
+public class MarsRoversTest
+{
+    
+    //La meseta debe tener dimensiones especificas (x e y).
+    [Fact]
+    public void La_meseta_debe_tener_dimensiones_especificas()
+    {
+        //Arrange
+        var coordenadaMaximaX = 5;
+        var coordenadaMaximaY = 5;
+
+        //Act
+        var meseta = new Meseta(coordenadaMaximaX, coordenadaMaximaY);
+        
+        //Assert
+        meseta.CoordenadaMaximaX.Should().Be(coordenadaMaximaX);
+        meseta.CoordenadaMaximaY.Should().Be(coordenadaMaximaY);
+    }
+}
+
+public class Meseta
+{
+
+    public int CoordenadaMaximaX { get; set; } = 5;
+    public int CoordenadaMaximaY { get; set; } = 5;
+    public Meseta(int coordenadaMaximaX, int coordenadaMaximaY)
+    {
+        
+    }
+
+}
---
+++ MarsRover/TestList.md
@@ -0,0 +1,13 @@
+﻿## Tests
+
+- [x] La meseta debe tener dimensiones especificas (x e y).
+- [ ] Los rovers deben empezar su recorrido en las posiciones y orientacion proporcionada (1 2 N - 3 3 E)
+- [ ] Los rovers pueden avanzar al frente con la instrucción 'M'.
+- [ ] Los rovers pueden girar a la izquierda con la instrucción 'L'.
+- [ ] Los rovers pueden girar a la derecha con la instrucción 'R'.
+- [ ] Los rovers deben recibir una secuencia con lo que realizarán el recorrido. 
+- [ ] Al menos dos rovers pueden realizar recorridos.
+- [ ] No se pueden mover dos rovers en paralelos.
+
+## Definiciones
+
